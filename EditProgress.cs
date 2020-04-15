8f5d0aa32617539ccc518b2bed0ba2253a5c914e
AstasiMe <62290689+AstasiMe@users.noreply.github.com>
2020-04-15 17:17:20 +0300

    Add files via upload

---
+++ EditProgress.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba_linq_1
+{
+    public partial class EditProgress : Form
+    {
+        public demo1Entities1 db = new demo1Entities1();
+        students item;
+       
+        public EditProgress(students stud)
+        {
+           
+            item = stud;
+            InitializeComponent();
+
+            var groups_for_list = (from g in db.groups
+                                   select g.name_group).Distinct();
+
+            foreach (string it in groups_for_list)
+            {
+                comboBox1.Items.Add(it);
+            }
+            textBox1.Text = item.surname.ToString();
+            textBox2.Text = item.name.ToString();
+
+            var query = (from g in db.groups
+                         where g.code_group == item.code_group
+                         select g.name_group).ToList();
+
+            var prog_for_list = (from p in db.progress
+                                 select p.estimate).Distinct();
+
+            comboBox1.SelectedItem = query[0];
+         
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var result = ((Form2)Owner).db.students.SingleOrDefault(w => w.code_stud == item.code_stud);
+            var res = ((Form2)Owner).db.progress.SingleOrDefault(w => w.code_stud == item.code_stud && w.code_subject == comboBox3.SelectedIndex);
+            result.surname = textBox1.Text.ToString().Trim();
+            result.name = textBox2.Text.ToString().Trim();
+
+            var query = (from g in db.groups
+                         where g.name_group == comboBox1.SelectedItem.ToString()
+                         select g.code_group).ToList();
+
+            var prog = (from p in db.progress
+                        where p.estimate == comboBox2.SelectedIndex 
+                        select p.code_stud).ToList();
+
+            result.code_group = query[0];
+            res.estimate = prog[0];
+            ((Form2)Owner).db.SaveChanges();
+            ((Form2)Owner).studentprogress = ((Form2)Owner).db.students.OrderBy(o => o.code_stud).ToList();
+           
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void EditProgress_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Menu.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace laba_linq_1
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.Location = new System.Drawing.Point(75, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Логин";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.Location = new System.Drawing.Point(75, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 33);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Пароль";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 53);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ВХОД";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(163, 138);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(213, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(47, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(339, 187);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.WaitOnLoad = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 63);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Список студентов";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(144, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 63);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Успеваемость";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(302, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(134, 63);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Выход";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 280);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Menu";
+            this.Text = "EDU";
+            this.Load += new System.EventHandler(this.Menu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
